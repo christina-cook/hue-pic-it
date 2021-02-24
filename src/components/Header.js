86599612fe66a6fd8ee0b22872b54799ab1c805e
srslie <aliceruppert@gmail.com>
2021-02-24 16:33:13 -0700

    Add bootstrap and header navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import React, {useState, useEffect} from 'react';
+import {Navbar, NavDropdown} from 'react-bootstrap'
+// import NavDropdown from 'react-bootstrap/NavDropdown'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
-    <h1>Hue Pic It</h1>
+    <Navbar bg="dark" expand="lg" fixed="top" variant="dark">
+      <Navbar.Brand as={Link} to="/">Hue Pic It</Navbar.Brand>
+      <NavDropdown title="User" id="basic-nav-dropdown">
+        <NavDropdown.Item as={Link} to="/">Account</NavDropdown.Item>
+        <NavDropdown.Item as={Link} to="/">Favorites</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item >Sign Out</NavDropdown.Item>
+      </NavDropdown>
+    </Navbar>
+
   )
 }
 
