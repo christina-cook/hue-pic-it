990a9da289a45df6d4a4457fc284ef7fde2f5a95
srslie <aliceruppert@gmail.com>
2021-02-25 13:16:38 -0700

    Add accessibility tag in loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,8 @@ import Spinner from 'react-bootstrap/Spinner'
 
 const Loading = () => {
   return (
-    <div className="loading" style={{height: "100vh", width: "100vw", display: "flex",alignItems: "center", justifyContent: "center"}} fluid>
+    <div className="loading" style={{height: "100vh", width: "100vw", display: "flex",alignItems: "center", justifyContent: "center"}} fluid role="status">
+    <span className="sr-only">Loading...</span>
       <Spinner className="dot mr-3" animation="grow" variant="danger" />
       <Spinner className="dot mr-3" animation="grow" variant="success" />
       <Spinner className="dot mr-3" animation="grow" variant="primary"  />
