89dff7a5c4054b7c2928f4c0dbdf479cc347ec29
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-02-24 18:56:59 -0700

    Add styling and Route Render

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ import ResultsDisplay from './ResultsDisplay';
 import Error from './Error';
 import Loading from './Loading';
 import AuthProvider from '../contexts/AuthContext';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
   return (
@@ -17,10 +18,14 @@ const App = () => {
     <AuthProvider>
       <Header />
         <Switch>
+          <Route exact path='/' component={Dashboard}/>
           <Route path='/login' component={Login}/>
           <Route path='/signup' component={Signup}/>
-          <Route exact path='/ '  component={Dashboard}/>
-          <Route path='/results' component={ResultsDisplay}/>
+          <Route path='/results/:color' 
+            render={({match}) => {
+              return <ResultsDisplay color={`${match.params.color}`} />
+            }}
+          />
         </Switch>
     </AuthProvider>
     <Footer />
