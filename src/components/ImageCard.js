b9538e2ec0fed340642250a6b6395512d35b1aa3
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-02-24 18:57:51 -0700

    Add display to image search results

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,8 +1,21 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
 
-const ImageCard = () => {
+const ImageCard = ({info}) => {
+  const {alt, color, description, id, image, photographer, download} = info
   return (
-    <h2>Image Card</h2>
+    <Card style={{ width: '18rem' }} key={id} id={id}>
+      <Card.Img variant="top" src={image} alt={alt} />
+      <Card.Body>
+        <Card.Title>{alt}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Card.Text><span>Hex: </span>{color} </Card.Text>
+        <Card.Text><span>Photographer: </span>{photographer} </Card.Text>
+      </Card.Body>
+      <a href={download}>
+        <Button style={{backgroundColor: 'white', fontSize: '2rem'}}>⬇️</Button>
+      </a>
+    </Card>
   )
 }
 
