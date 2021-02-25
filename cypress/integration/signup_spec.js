712add0b345e3c6d726a76a8189ec5bf0cfa2450
Christina Cook <cmc.cook7@gmail.com>
2021-02-25 14:12:10 -0700

    Set up testing files for forms

---
+++ cypress/integration/signup_spec.js
@@ -0,0 +1,18 @@
+describe('Signup page', () => {
+  const baseURL = 'http://localhost:3000/'
+  beforeEach(() => {
+    cy.visit(baseURL)
+  })
+
+  it('Should display a header at the top of the page with app name and menu', () => {
+
+  })
+
+  it('Should display a footer at the bottom of the page with links to about section and GitHub accounts', () => {
+
+  })
+
+  it('Should display a signup form', () => {
+
+  })
+})
