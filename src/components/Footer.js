a4bdb1d57388bceaa5c8e74c3a68f49a9285da7f
srslie <aliceruppert@gmail.com>
2021-02-25 09:05:33 -0700

    Add developer pics

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,11 +7,14 @@ const Footer = () => {
   return (
     <Navbar bg="dark" expand="lg" fixed="bottom" variant="dark">
       <Navbar.Brand as={Link} to="/about">About Site</Navbar.Brand>
-      <Navbar.Brand href="https://github.com/christina-cook">Christina</Navbar.Brand>
+      <Navbar.Brand href="https://github.com/christina-cook">
+        <Image src={require('../assets/christina.jpg').default} width={40} height={40} roundedCircle fluid/> Christina
+      </Navbar.Brand>
       <Navbar.Brand href="https://github.com/srslie">
-      <Image src={require('../assets/alice.jpg')} roundedCircle />
-        Alice</Navbar.Brand>
-      <Navbar.Brand href="https://github.com/kevinhartmann23">Kevin</Navbar.Brand>
+        <Image src={require('../assets/alice.jpg').default} width={40} height={40} roundedCircle fluid/> Alice
+      </Navbar.Brand>
+      <Navbar.Brand href="https://github.com/kevinhartmann23">
+        <Image src={require('../assets/kevin.jpg').default} width={40} height={40} roundedCircle fluid/> Kevin</Navbar.Brand>
     </Navbar>
   )
 }
