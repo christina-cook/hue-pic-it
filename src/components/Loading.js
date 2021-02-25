45edf8a76a74510de7c3a18b32249f3ae1576084
srslie <aliceruppert@gmail.com>
2021-02-25 13:13:48 -0700

    Add basic spinnners in loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,8 +1,14 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import Spinner from 'react-bootstrap/Spinner'
 
 const Loading = () => {
   return (
-    <h2>Loading</h2>
+    <div className="loading" style={{height: "100vh", width: "100vw", display: "flex",alignItems: "center", justifyContent: "center"}} fluid>
+      <Spinner className="dot mr-3" animation="grow" variant="danger" />
+      <Spinner className="dot mr-3" animation="grow" variant="success" />
+      <Spinner className="dot mr-3" animation="grow" variant="primary"  />
+      <Spinner className="dot" animation="grow" variant="secondary"  />   
+    </div>
   )
 }
 
