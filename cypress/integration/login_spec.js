7cd635bc2383dc1632a355168819857e6f33328d
Christina Cook <cmc.cook7@gmail.com>
2021-02-25 21:41:47 -0700

    Test visibility of signup form

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,18 +1,10 @@
 describe('Login page', () => {
-  const baseURL = 'http://localhost:3000/'
+  const baseURL = 'http://localhost:3000/login'
   beforeEach(() => {
     cy.visit(baseURL)
   })
 
-  it('Should display a header at the top of the page with app name and menu', () => {
+  it('Should display a login form on the page', () => {
 
-  })
-
-  it('Should display a footer at the bottom of the page with links to about section and GitHub accounts', () => {
-
-  })
-
-  it('Should display a login form', () => {
-    
   })
 })
--- cypress/integration/signup_spec.js
+++ cypress/integration/signup_spec.js
@@ -1,18 +1,11 @@
 describe('Signup page', () => {
-  const baseURL = 'http://localhost:3000/'
+  const baseURL = 'http://localhost:3000/signup'
   beforeEach(() => {
     cy.visit(baseURL)
   })
 
-  it('Should display a header at the top of the page with app name and menu', () => {
-
+  it('Should display a signup form on the page', () => {
+    cy.get('.form-card').find('.signup-form').should('contain', 'Sign Up')
   })
 
-  it('Should display a footer at the bottom of the page with links to about section and GitHub accounts', () => {
-
-  })
-
-  it('Should display a signup form', () => {
-
-  })
 })
