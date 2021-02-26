3127d1f5e69b02b27a93bb9dad882e54d55754c3
Christina Cook <cmc.cook7@gmail.com>
2021-02-25 22:00:24 -0700

    Test form input visibility

--- cypress/integration/signup_spec.js
+++ cypress/integration/signup_spec.js
@@ -5,7 +5,16 @@ describe('Signup page', () => {
   })
 
   it('Should display a signup form on the page', () => {
-    cy.get('.form-card').find('.signup-form').should('contain', 'Sign Up')
+    cy.get('.form-card').find('.signup-form').should('be.visible').and('contain', 'Sign Up')
   })
 
+  it.only('Should have inputs for email, password, and password confirmation', () => {
+    cy.get('input[type=email]').should('exist')
+      .get('.form-label').should('have.length', '3')
+      .get('input[type=password]').should('exist').and('have.length', '2')
+  })
+
+  it('Should see an error message if the passwords don\'t match', () => {
+
+  })
 })
