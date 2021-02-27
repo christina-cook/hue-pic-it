61badeeb272b659b5380ddbd8ca30335b4f4491e
srslie <aliceruppert@gmail.com>
2021-02-27 13:46:06 -0700

    Play with boxShadow

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,7 +4,7 @@ import { Card, Button } from 'react-bootstrap';
 const ImageCard = ({info}) => {
   const {alt, color, description, id, image, photographer, download} = info
   return (
-    <Card style={{ width: '18rem' }} key={id} id={id}>
+    <Card style={{ width: '100%', boxShadow: `5px 5px 20px 0px ${color}`}} key={id} id={id}>
       <Card.Img variant="top" src={image} alt={alt} />
       <Card.Body>
         <Card.Title>{alt}</Card.Title>
