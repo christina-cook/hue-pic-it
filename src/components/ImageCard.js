54cc3f55a016685909231e3aeb30bf423dde3531
srslie <aliceruppert@gmail.com>
2021-02-27 14:55:37 -0700

    Change imageCard to only show image

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,17 +4,17 @@ import { Card, Button } from 'react-bootstrap';
 const ImageCard = ({info}) => {
   const {alt, color, description, id, image, photographer, download} = info
   return (
-    <Card style={{ width: '100%', boxShadow: `5px 5px 20px 0px ${color}`}} key={id} id={id}>
-      <Card.Img variant="top" src={image} alt={alt} />
-      <Card.Body>
+    <Card style={{ width: 'auto', height: '100%', border: 'none'}} key={id} id={id}>
+      <Card.Img style={{objectFit: 'cover', justifyContent: 'center', height: '100%', width: '100%'}} src={image} alt={alt} fluid/>
+      {/* <Card.Body style={{ height: '25%'}}>
         <Card.Title>{alt}</Card.Title>
         <Card.Text>{description}</Card.Text>
-        <Card.Text><span>Hex: </span>{color}</Card.Text>
+        <Card.Text><span>Hex: </span>{color.toUpperCase()}</Card.Text>
         <Card.Text><span>Photographer: </span>{photographer} </Card.Text>
-      </Card.Body>
-      <a href={download}>
+      </Card.Body> */}
+      {/* <a href={download}>
         <Button style={{backgroundColor: 'white', fontSize: '2rem'}}>⬇️</Button>
-      </a>
+      </a> */}
     </Card>
   )
 }
