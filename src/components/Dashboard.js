a22e9b360af6dfd10a2d64cbad1737c718533a8b
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-02-26 17:01:06 -0700

    Add card styling to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,19 @@
 import React, {useState, useEffect} from 'react';
-import ColorCard from './ColorCard';
+import ColorCard from './ColorCard'
+import cards from './colorCardInfo'
+import './Dashboard.css'
 import { Container } from 'react-bootstrap';
 
 const Dashboard = () => {
-  const colorKeys = ['red', 'green', 'blue', 'black and white']
-  const cards = colorKeys.map(color => {
+  const colorKeys = Object.keys(cards)
+  const colorCards = colorKeys.map(color => {
     return <ColorCard key={color} id={color} />
   })
 
   return (
     <>
-      <Container className='d-flex w-100'>
-        {cards}
+      <Container className='card-container'>
+        {colorCards}
       </Container>
     </>
   )
---
+++ src/components/colorCardInfo.js
@@ -0,0 +1,74 @@
+const cards = {
+  red: {
+    name: "Red",
+    text: "love, immediacy, sale, passion, hunger",
+    colors: {
+      light: "#F8EAEC",
+      soft: "#D7868F",
+      strong: "#BD3645",
+      font: "#120506",
+    }
+  },
+  orange: {
+    name: "Orange",
+    text: "health, attraction, youthful, thirst, happiness",
+    colors: {
+      light: "#FFF1EB",
+      soft: "#FFAB87",
+      strong: "#FF7338",
+      font: "#190B05",
+    }
+  },
+  yellow: {
+    name: "Yellow",
+    text: "cheer, fresh, attention, energy, optimism",
+    colors: {
+      light: "#FDFBED",
+      soft: "#F7EC94",
+      strong: "#F3E04E",
+      font: "#181607",
+    }
+  },
+  green: {
+    name: "Green",
+    text: "eco-friendly, soothing, natural, balance, restful",
+    colors: {
+      light: "#F4F7EE",
+      soft: "#BDD29A",
+      strong: "#92B558",
+      font: "#0E1208"
+    }
+  },
+  blue: {
+    name: "Blue",
+    text: "trust, calm, smart, faith, natural, stable",
+    colors: {
+      light: "#EAEEF3",
+      soft: "#8599B9",
+      strong: "#34558B",
+      font: "#05090D",
+    }
+  },
+  violet: {
+    name: "Violet",
+    text: "royal, mysterious, luxury, childish",
+    colors: {
+      light: "#F0EEF4",
+      soft: "#A69CBF",
+      strong: "#6B5B95",
+      font: "#0A090E",
+    }
+  },
+  black_and_white: {
+    name: "Black & White",
+    text: "bold, rich, power, mystery, elegance, strength",
+    colors: {
+      light: "#E5E5E5",
+      soft: "#666666",
+      strong: "#000000",
+      font: "#191919",
+    }
+  }
+}
+
+export default cards
