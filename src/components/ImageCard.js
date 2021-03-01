bd41323a82c9ece3ac9c7b5331bf29d119f4074e
Christina Cook <cmc.cook7@gmail.com>
2021-03-01 14:52:38 -0700

    Fix error on image download

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -15,6 +15,11 @@ const ImageCard = ({info}) => {
     }
   }
 
+  const handleDownload = event => {
+    event.preventDefault()
+    window.open(download, '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
+  }
+
   return (
     <Card style={{border: 'none'}} key={id} id={id}>
       <Card.Img style={{objectFit: 'cover', justifyContent: 'center', height: '100%', width: '100%'}} onClick={handleClick} src={image} alt={alt} fluid/>
@@ -32,10 +37,10 @@ const ImageCard = ({info}) => {
           <span>
           <i className="bi bi-camera-fill" title="Photographer"/>
           </span>
-          {photographer} 
+          {photographer}
         </Card.Text>
       <a href={download}>
-        <Button onClick={`window.location=${download}`}>
+        <Button onClick={handleDownload}>
           <i className="bi bi-download" /> Download
         </Button>
       </a>
