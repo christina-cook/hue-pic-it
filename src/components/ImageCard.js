aa0b4fa43d94977e9213c6e4cbc1e06982d3e32c
srslie <aliceruppert@gmail.com>
2021-03-01 11:08:30 -0700

    Add click to show info

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './ImageCard.css'
 import { Card, Button } from 'react-bootstrap';
 
 const ImageCard = ({info}) => {
+  const [hidden, setHidden] = useState('hidden')
   const {alt, color, description, id, image, photographer, download} = info
+
+  const handleClick = event => {
+    event.preventDefault()
+    if (hidden) {
+      setHidden('')
+    } else {
+      setHidden('hidden')
+    }
+  }
+
   return (
-    <Card style={{ width: 'auto', height: '100%', border: 'none'}} key={id} id={id}>
-      <Card.Img style={{objectFit: 'cover', justifyContent: 'center', height: '100%', width: '100%'}} src={image} alt={alt} fluid/>
-      {/* <Card.Body style={{ height: '25%'}}>
+    <Card className="cardyB" style={{border: 'none'}} key={id} id={id}>
+      <Card.Img style={{objectFit: 'cover', justifyContent: 'center', height: '100%', width: '100%', zIndex: "11"}} onClick={handleClick} src={image} alt={alt} fluid/>
+      <Card.ImgOverlay className={`card-info ${hidden}`} onClick={handleClick}>
         <Card.Title>{alt}</Card.Title>
         <Card.Text>{description}</Card.Text>
         <Card.Text><span>Hex: </span>{color.toUpperCase()}</Card.Text>
         <Card.Text><span>Photographer: </span>{photographer} </Card.Text>
-      </Card.Body> */}
-      {/* <a href={download}>
-        <Button style={{backgroundColor: 'white', fontSize: '2rem'}}>⬇️</Button>
-      </a> */}
+      <a href={download}>
+        <Button onClick={`window.location=${download}`}>
+          <i className="bi bi-download" />
+        </Button>
+      </a>
+      </Card.ImgOverlay>
     </Card>
   )
 }
