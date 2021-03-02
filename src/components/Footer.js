52c9711df21fd36b0dcbe4c9e45a851429593e4d
srslie <aliceruppert@gmail.com>
2021-03-02 11:34:16 -0700

    Add conditional rendering for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Image from 'react-bootstrap/Image';
 import {Link} from 'react-router-dom';
+import {useAuth} from '../contexts/AuthContext';
 
 const Footer = () => {
+  const {currentUser} = useAuth()
+
+
   return (
+    <>
+    {currentUser && 
     <Navbar className="footer" bg="dark" expand="lg" fixed="bottom" variant="dark">
       <Navbar.Brand className="aboutLink"as={Link} to="/about" style={{fontFamily: "Bungee Inline, cursive"}}>About Site</Navbar.Brand>
       <Navbar.Brand  className="christina" href="https://github.com/christina-cook" style={{fontFamily: "Bungee Inline, cursive"}}>
@@ -16,6 +22,8 @@ const Footer = () => {
       <Navbar.Brand className="kevin" href="https://github.com/kevinhartmann23" style={{fontFamily: "Bungee Inline, cursive"}}>
         <Image src={require('../assets/kevin.jpg').default} width={40} height={40} roundedCircle fluid/> Kevin</Navbar.Brand>
     </Navbar>
+    }
+    </>
   )
 }
 
